841c6aa4bd6e3d2b9b8b95318b20b309ae3795c0
bunnyBites <s.mukeshkr481@gmail.com>
2023-06-13 23:31:38 +0530

    add background

---
+++ lib/screens/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.deepPurple.shade100,
+              Colors.deepPurple.shade200,
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
