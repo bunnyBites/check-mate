1017f335a62b016cf7cd4cf7df0c6adeb8e75a98
bunnyBites <s.mukeshkr481@gmail.com>
2023-07-02 08:30:20 +0530

    modified color

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -19,7 +19,7 @@ class _HomeState extends State<Home> {
             end: Alignment.bottomCenter,
             colors: [
               Colors.deepPurple.shade300,
-              Colors.deepPurple.shade200,
+              Colors.deepPurple.shade500,
             ],
           ),
         ),
